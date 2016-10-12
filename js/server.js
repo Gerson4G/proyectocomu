20602c31e23a50847aca6e2606692bf893f59071
gersongoatache@gmail.com <gersongoatache@gmail.com>
2016-10-12 02:54:30 +0000

    POO en un puerto TCP

--- js/server.js
+++ js/server.js
@@ -6,6 +6,7 @@ var enviarPuerto = false;
 var cerrado= false;
 var puertoTCP=7777;
 var tiempoEnvio=1000;
+var nombre;
 
 function Maquina(tiempo,puerto,nombre,ip) {
   this.tiempo=tiempo;
@@ -16,6 +17,8 @@ function Maquina(tiempo,puerto,nombre,ip) {
 }
 
 function iniciar() {
+
+TCP(ip.address,puertoTCP)
 
   var dgram = require('dgram');
   var socket = dgram.createSocket('udp4');
@@ -30,19 +33,20 @@ function iniciar() {
   });
   socket.on('message', (msg, rinfo) => {
     console.log("Message received from " + rinfo.address);
-
-    listaM.push(new Maquina(0,abrirPuerto(),msg.toString(),rinfo.address));
+    nombre=msg.toString();
+  //  listaM.push(new Maquina(0,rinfo.port,msg.toString(),rinfo.address));
     //listaM.push(new Maquina(0,puertoTCP,msg.toString(),rinfo.address));
 
-    TCP(ip.address,listaM[listaM.length-1].puerto);
+/* SI LO QUIERO EN DIFERENTES PUERTOS EL TCP*/
+    //TCP(ip.address,listaM[listaM.length-1].puerto);
     //TCP(ip.address,puertoTCP)
     enviarPuerto = true;
 
     if(enviarPuerto){
-      console.log('ENVIANDO PUERTO '+listaM[listaM.length-1].puerto);
-      var mensaje = listaM[listaM.length-1].puerto.toString();
-      //var mensaje = puertoTCP.toString();
-      socket.send(new Buffer (mensaje), 0, mensaje.length, rinfo.port , listaM[listaM.length-1].ip, function(err, bytes) {
+      //console.log('ENVIANDO PUERTO '+listaM[listaM.length-1].puerto);
+      //var mensaje = listaM[listaM.length-1].puerto.toString();
+      var mensaje = puertoTCP.toString();
+      socket.send(new Buffer (mensaje), 0, mensaje.length, rinfo.port , rinfo.address, function(err, bytes) {
         if (err) throw err;
         console.log('Puerto enviado');
       });
@@ -68,9 +72,11 @@ function iniciar() {
 
 function TCP(ip,port_tcp){
 
-    var pos=listaM.length-1;
+
     serverTCP = net.createServer(function(socket) {
+    var pos=listaM.length-1;
     console.log('client connected');
+    listaM.push(new Maquina(0,socket.remotePort,nombre,socket.remoteAddress));
     listaM[listaM.length-1].tiempo=new Date().getTime();
 
     socket.on('error', (e) => {
@@ -102,12 +108,14 @@ function abrirPuerto() {
 
 function registroTiempo(socket, pos){
 
-if(!cerrado)
-  listaM[pos].tiempo=0;
-else {
   var aux=new Date();
-  listaM[pos].cerrado=true;
-  listaM[pos].tiempo=(aux.getTime()-listaM[pos].tiempo)/1000;
+  try {
+    listaM[pos].cerrado=true;
+    listaM[pos].tiempo=(aux.getTime()-listaM[pos].tiempo)/1000;
+  }
+    catch(err) {
+        console.log();
+  }
+
   cerrado=false;
-}
 }
